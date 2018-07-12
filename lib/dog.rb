4267dbbcf1cf0fd7fc14fa480b58dfc0c956d1e2
akgmarquez <akgmarquez@gmail.com>
2018-07-12 22:29:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # dog.rb
+
+class Dog
+    def initialize(name, breed)
+      @name = name
+      @breed = breed
+    end
+    
+    def name
+      @name = name
+    end
+    
+    def name=(new_name)
+      @name = new_name
+    end
+    
+    def breed
+      @breed = breed
+    end
+    
+    def breed=(new_breed)
+      @breed = new_breed
